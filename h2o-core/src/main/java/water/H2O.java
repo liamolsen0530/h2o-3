16d02eba44ebc71c5bb0bb73e18585da0c825f2a
Raymond Peck <rpeck@rpeck.com>
2015-07-10 22:32:17 +0200

    PUBDEV-1093: Add warnings/errors support for generic jobs

--- h2o-core/src/main/java/water/H2O.java
+++ h2o-core/src/main/java/water/H2O.java
@@ -1,5 +1,6 @@
 package water;
 
+import com.brsanthu.googleanalytics.GoogleAnalytics;
 import hex.ModelBuilder;
 import jsr166y.CountedCompleter;
 import jsr166y.ForkJoinPool;
@@ -16,8 +17,8 @@ import water.persist.PersistManager;
 import water.util.DocGen.HTML;
 import water.util.GAUtils;
 import water.util.Log;
-import water.util.PrettyPrint;
 import water.util.OSUtils;
+import water.util.PrettyPrint;
 
 import java.io.File;
 import java.lang.management.ManagementFactory;
@@ -29,7 +30,6 @@ import java.util.*;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Future;
 import java.util.concurrent.atomic.AtomicLong;
-import com.brsanthu.googleanalytics.GoogleAnalytics;
 
 /**
 * Start point for creating or joining an <code>H2O</code> Cloud.
@@ -615,7 +615,7 @@ final public class H2O {
     apisRegistered = true;
 
     long registerApisMillis = System.currentTimeMillis() - before;
-    Log.info("Registered REST APIs in: " + registerApisMillis + "mS");
+    Log.info("Registered: " + RequestServer.numRoutes() + " REST APIs in: " + registerApisMillis + "mS");
   }
 
   //-------------------------------------------------------------------------------------------------------------------
